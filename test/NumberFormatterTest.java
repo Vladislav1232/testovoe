c0f1aa2e71c308dc28dad4d6ac4bfb69edced7c7
Vladislav1232 <60581238+Vladislav1232@users.noreply.github.com>
2021-08-25 19:43:26 +0300

    Update NumberFormatterTest.java

--- test/NumberFormatterTest.java
+++ test/NumberFormatterTest.java
@@ -1,33 +1,30 @@
 import org.junit.jupiter.api.Test;
-
-import java.text.ParseException;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberFormatterTest {
 
     @Test
-    void numberFormatTranslatorMethod1() throws ParseException {
+    void numberFormatTranslatorMethod1() {
         assertEquals(15, NumberFormatter.parseInt("+10") +5);
     }
 
     @Test
-    void numberFormatTranslatorMethod2() throws ParseException {
+    void numberFormatTranslatorMethod2() {
         assertEquals(-7, NumberFormatter.parseInt("-10") +3);
     }
 
     @Test
-    void numberFormatTranslatorMethod3() throws ParseException {
+    void numberFormatTranslatorMethod3() {
         assertEquals(24690, NumberFormatter.parseInt("12345") * 2);
     }
 
     @Test
-    void numberFormatTranslatorMethod4() throws ParseException {
+    void numberFormatTranslatorMethod4() {
         assertEquals(12346, NumberFormatter.parseInt("12345") +1);
     }
 
     @Test
-    void numberFormatTranslatorMethod5() throws ParseException {
+    void numberFormatTranslatorMethod5() {
         assertEquals(-12344, NumberFormatter.parseInt("-12345") +1);
     }
 }
